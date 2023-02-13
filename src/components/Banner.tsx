bfa75ac826613b2406e777d89b524ae809de91ba
NTBANG <nguyenthanhbanng0901@gmail.com>
2023-02-13 22:53:45 +0700

    day 1

---
+++ src/components/Banner.tsx
@@ -0,0 +1,53 @@
+import { baseUrl } from "constants/movie";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Movie } from "typing";
+import { FaPlay } from "react-icons/fa";
+import { InformationCircleIcon } from "@heroicons/react/24/solid";
+
+interface Props {
+  netflixOriginals: Movie[];
+}
+
+function Banner({ netflixOriginals }: Props) {
+  const [movie, setMovie] = useState<Movie | null>(null);
+
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    );
+  }, [netflixOriginals]);
+
+  console.log(movie);
+
+  return (
+    <div className="flex flex-col space-x-2 py-16 md:space-x-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+      <div className="absolute top-0 left-0 h-[95vh] -z-10 w-screen">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          alt={movie?.title as string}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl lg:mb-3 ">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl mb-2 lg:mb-4">
+        {movie?.overview}
+      </p>
+
+      <div className="flex space-x-3">
+        <button className="banner-btn bg-white text-black">
+          <FaPlay className="w-4 h-4 text-black md:w-7 md:h-7" /> Play
+        </button>
+        <button className="banner-btn bg-[gray]/70">
+          <InformationCircleIcon className="h-5 w-5 md:w-8 md:h-8" />
+          More Info
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
---
+++ src/components/Header.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import { BellIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <Image
+          src="/logo.svg"
+          alt="Logo"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="header-link">Home</li>
+          <li className="header-link">TV Shows</li>
+          <li className="header-link">Movies</li>
+          <li className="header-link">New & Popular</li>
+          <li className="header-link">My List</li>
+        </ul>
+      </div>
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <MagnifyingGlassIcon className=" hidden sm:inline w-6 h-6" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="w-6 h-6" />
+        <Link href="/account">
+          <Image
+            src="/images/defaultAvatar.png"
+            alt="Avatar"
+            width={32}
+            height={32}
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
